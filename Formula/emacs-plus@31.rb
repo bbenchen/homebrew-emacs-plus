a100367a057872b2b878ba29d8f02efd70f64bd0
Ben Chen <517926804@qq.com>
2024-12-20 13:39:19 +0800

    fix round-undecorated-frame.patch for emacs 31

--- Formula/emacs-plus@31.rb
+++ Formula/emacs-plus@31.rb
@@ -99,7 +99,7 @@ class EmacsPlusAT31 < EmacsBase
   opoo "The option --with-no-frame-refocus is not required anymore in emacs-plus@31." if build.with? "no-frame-refocus"
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
   local_patch "system-appearance", sha: "9eb3ce80640025bff96ebaeb5893430116368d6349f4eb0cb4ef8b3d58477db6"
-  local_patch "round-undecorated-frame", sha: "014ce732beefa0fab53f6e8d630593b73a83e96518a97f6316850295f01231b1"
+  local_patch "round-undecorated-frame", sha: "5a26c4f5b7252a25e15fb7c34fa608ad6739b96cb0f4d2b37a01391820a17fb8"
 
   if build.with? "alpha-background"
     local_patch "alpha-background", sha: "3b7dfcefdf60c8ade66ac9818bdc0d83b90cb61bf0d3195b2c8d923c4714a681"
