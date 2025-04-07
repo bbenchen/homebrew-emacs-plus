e13fe4211b25700ae505d962a182de3a0d286305
Ben Chen <517926804@qq.com>
2025-04-07 10:07:04 +0800

    Fix sha256 code for emacs31

--- Formula/emacs-plus@31.rb
+++ Formula/emacs-plus@31.rb
@@ -94,7 +94,7 @@ class EmacsPlusAT31 < EmacsBase
   opoo "The option --with-no-frame-refocus is not required anymore in emacs-plus@31." if build.with? "no-frame-refocus"
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
   local_patch "system-appearance", sha: "53283503db5ed2887e9d733baaaf80f2c810e668e782e988bda5855a0b1ebeb4"
-  local_patch "round-undecorated-frame", sha: "26947b6724fc29fadd44889808c5cf0b4ce6278cf04f46086a21df50c8c4151d"
+  local_patch "round-undecorated-frame", sha: "32a11412552de126d537ac6e1ad1d5455bf78fa16ea6ab12e5e4c3c6f278276b"
 
   if build.with? "alpha-background"
     local_patch "alpha-background", sha: "3b7dfcefdf60c8ade66ac9818bdc0d83b90cb61bf0d3195b2c8d923c4714a681"
